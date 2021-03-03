474f04fcd33ac0b9cb6b5aae7c952f1f7a55e88e
Mohamed Abdelnasser <mohamedabdelnasser@MacBook-Air.local>
2021-03-03 13:33:07 +0000

    add nav-links hover

--- main.js
+++ main.js
@@ -2,18 +2,15 @@
 // Navigation toggle 
 const toggle= document.querySelector(".hamburger");
 const navLinks= document.querySelector(".nav-links");
-const bodyEl = document.querySelector("body");
 
 toggle.addEventListener("click", ()=>{
     if(toggle.classList.contains("active")){
         toggle.classList.remove("active")
         toggle.setAttribute("src" ,"./images/icon-close.svg");
         navLinks.classList.add("show-nav");
-        bodyEl.classList.add("body-height");
     }else{
         toggle.classList.add("active");
         toggle.setAttribute("src" ,"./images/icon-hamburger.svg")
         navLinks.classList.remove("show-nav");
-        bodyEl.classList.remove("body-height");
     }
 })
